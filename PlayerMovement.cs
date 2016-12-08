a4309e89fb80b40329774ce1427bc0cc4ab6910b
Chris Whiting <mpro34@gmail.com>
2016-12-07 21:06:05 -0800

    New additions for player movement and shooting

---
+++ PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	//Fires every Physics updates
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		//Call all helper functions.
+		Move (h, v);
+		Turning ();
+		Animating (h, v);
+	}
+
+	void Move(float h, float v) 
+	{
+		movement.Set (h, 0f, v);  //Set the Vector3 variable movement
+		movement = movement.normalized * speed * Time.deltaTime; //Speed is scale, Time updated every FixedUpdate.
+
+		playerRigidbody.MovePosition (transform.position + movement); //Current position + new addition above.
+	}
+
+	//Uses rays to initiate mouse following.
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition); //Find the point underneath the mouse if it hits the floor Quad.
+
+		RaycastHit floorHit;
+		//out means the floorHit will be assigned values.
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) 
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;  //h not equal to 0 or v not equal to zero.
+		anim.SetBool("IsWalking", walking);
+	}
+}
---
+++ PlayerShooting.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+
+
+    float timer;
+    Ray shootRay = new Ray();
+    RaycastHit shootHit;
+    int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+
+
+    void Awake ()
+    {
+        shootableMask = LayerMask.GetMask ("Shootable"); //Can only shoot Game Objects on this Shootable layer.
+        gunParticles = GetComponent<ParticleSystem> ();
+        gunLine = GetComponent <LineRenderer> ();
+        gunAudio = GetComponent<AudioSource> ();
+        gunLight = GetComponent<Light> ();
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shoot ();
+        }
+
+		//If we fire and evnough time has progressed, we turn off the flash, sound, etc of the gunBarrelEnd.
+        if(timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects ();
+        }
+    }
+
+
+    public void DisableEffects ()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+
+    void Shoot ()
+    {
+        timer = 0f;
+
+        gunAudio.Play ();
+
+        gunLight.enabled = true;
+
+        gunParticles.Stop ();//Ensure that the particles are stopped before trying to play.
+        gunParticles.Play ();
+
+		//Turn on line renderer
+        gunLine.enabled = true;
+        gunLine.SetPosition (0, transform.position); //Set Start of gunLine to the end of gun barrel.
+
+        shootRay.origin = transform.position;  //Start ray at tip of gun
+		shootRay.direction = transform.forward;  //Direction that ray should shoot. (Z - AXIS)
+
+		//out shootHit gives info about what was hit by shootRay. shootableMask specifies the mask that the ray is able to hit. (Shootable Layer above)
+        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> (); //Give me the enemy health script Component.
+            if(enemyHealth != null) //If no health script returned, don't go through this loop.
+            {
+                enemyHealth.TakeDamage (damagePerShot, shootHit.point); //shootHit.point is the location where the stuffing particles get rendered.
+            }
+            gunLine.SetPosition (1, shootHit.point); //Start at the end of the gun, end at the shootable collider that was hit.
+        }
+        else
+        {
+			//Not shooting something in the Shootable Layer
+			//Start of Ray + Direction(Where mouse is pointed) * range of shot (100f)
+            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
